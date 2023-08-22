dc285d85f023386632029c08214dc4f185f3e80d
Martin Flasar <christian.flasar@gmail.com>
2023-08-23 01:15:50 +0200

    Contact and Projects section base structure added.

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,7 +4,15 @@ import "../../assets/components/Contact/Contact.scss"
 const Contact = () => {
   return (
     <section className="contact">
-      <h1>Contact</h1>
+      <h1>Contact Me</h1>
+      <form name='contact' method="POST" data-netlify="true" data-netlify-honeypot='bot-field'>
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" id="email" />
+        <label htmlFor="message">Message</label>
+        <textarea name="message" id="message"></textarea>
+        <button type="submit">Send</button>
+        <button type="button" disabled>Send via email client</button>
+      </form>
     </section>
   )
 }
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,9 +2,49 @@ import React from "react";
 import '../../assets/components/Projects/Projects.scss'
 
 const Projects = () => {
+  const projectData = [
+    {
+      id: 1,
+      image: "https://i.imgur.com/UqNMhpZ.png",
+      caption: "Tic Tac Toe",
+    },
+    {
+      id: 2,
+      image: "https://i.imgur.com/UqNMhpZ.png",
+      caption: "Tic Tac Toe",
+    },
+    {
+      id: 3,
+      image: "https://i.imgur.com/UqNMhpZ.png",
+      caption: "Tic Tac Toe",
+    },
+    {
+      id: 4,
+      image: "https://i.imgur.com/UqNMhpZ.png",
+      caption: "Tic Tac Toe",
+    },
+    {
+      id: 5,
+      image: "https://i.imgur.com/UqNMhpZ.png",
+      caption: "Tic Tac Toe",
+    }
+  ]
+
+  const handleItemClick = (project) => () => {
+    console.log(project)
+  }
+
   return (
     <section className="projects">
       <h1>Projects</h1>
+      <div className="project-grid">
+        {projectData.map((project) => (
+          <div className="project-item" key={project.id} onClick={handleItemClick(project)}>
+            <img src={project.image} alt={project.caption} />
+            <div className="caption">{project.caption}</div>
+          </div>
+        ))}
+      </div>
     </section>
   )
 }
