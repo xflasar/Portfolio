4faf62b8156336fd0a5314070301df6ad0beb70e
Martin Flasar <christian.flasar@gmail.com>
2023-11-12 05:39:32 +0100

    Added Internet Media ( LinkedIn, Github) links. Small styling modification.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import '../../assets/components/Footer/Footer.scss'
+
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
+        <a className='linkedin' href='https://www.linkedin.com/in/xflasar' rel='noreferrer' target='_blank'>
+          <img src='./assets/home/linkedin-img.webp' className='linkedin-img'/>
+        </a>
         <p className="footer-text">© 2023 Martin Flasar. All rights reserved. </p>
+        <a className='github' href='https://github.com/xflasar' rel='noreferrer' target='_blank'>
+          <img src='./assets/home/github-mark-white.webp' className='github-img'/>
+        </a>
       </div>
     </footer>
   )
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -226,18 +226,18 @@ const Home = () => {
       <button type='button' className='locale-button' onClick={(e) => handleLocaleChange(e)} value={locale === 'en-US' ? 'cs-CS' : 'en-US'}>{locale === 'en-US' ? 'CZ' : 'EN'}</button>
     </div>
         <div className="prof-image">
-          <img src="../prof-image.webp" alt='Profile picture'/>
+          <img src="./prof-image.webp" className='profile-img' alt='Profile picture'/>
           <div className='links'>
             <a href='#About' className={'about-nav load-in'} onClick={(e) => handleNavbar(e, 'about')}><span className='navBubble' onMouseEnter={handleNavAnimation} onAnimationEnd={handleNavAnimationEnd}/><span className='nav-title'>{localeData?.AboutLink}</span></a>
             <a href='#Skills' className="skills-nav load-in" onClick={(e) => handleNavbar(e, 'skills')}><span className='navBubble' onMouseEnter={handleNavAnimation} onAnimationEnd={handleNavAnimationEnd} /><span className='nav-title'>{localeData?.SkillsLink}</span></a>
             <a href='#Projects' className="projects-nav load-in" onClick={(e) => handleNavbar(e, 'projects')}><span className='navBubble' onMouseEnter={handleNavAnimation} onAnimationEnd={handleNavAnimationEnd} /><span className='nav-title'>{localeData?.ProjectsLink}</span></a>
             <a href='#Contact' className="contact-nav load-in" onClick={(e) => handleNavbar(e, 'contact')}><span className='navBubble' onMouseEnter={handleNavAnimation} onAnimationEnd={handleNavAnimationEnd} /><span className='nav-title'>{localeData?.ContactLink}</span></a>
+            {/* fix this style and look */ isMobile && <button type='button' className={githubActivityShow ? 'github-activity-button-show active' : 'github-activity-button-show'} onClick={handleGithubActivityShow}><img src='../assets/home/github-mark-white.webp' />GitHub Activity</button>}
           </div>
         </div>
         <div className={githubActivityShow ? 'github-container active' : isMobile ? 'github-container deactivated' : 'github-container'}>
           <GitActivity />
         </div>
-        {isMobile && <button type='button' className={githubActivityShow ? 'github-activity-button-show active' : 'github-activity-button-show'} onClick={handleGithubActivityShow}><img src='../assets/home/github-mark-white.png' />GitHub Activity</button>}
         <div className='home-intro-container'>
           <h1 className="home-intro-h1">{localeData.HomeIntroH1}</h1>
           <div className="home-intro">
