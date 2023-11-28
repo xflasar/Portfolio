8baffb4ed4106d60750d7683d7f8b77bd336ffea
Martin Flasar <christian.flasar@gmail.com>
2023-11-28 21:42:19 +0100

    Small Styling for viewer. ( Set General Style Design )

--- src/components/Projects/projectViewer.jsx
+++ src/components/Projects/projectViewer.jsx
@@ -10,12 +10,20 @@ const ProjectViewer = ({ project, closeViewer }) => {
   return (
     <div className="project-viewer">
       <button className="close-button" onClick={handleCloseViewer}>X</button>
-      <div className='image-slider'>
-        {/* here are gonna be an images sliding right to left if there are more than 2 */}
-      </div>
-      <div className='project-info'>
-        <h2>{project.projectName}</h2>
-        <p>{project.projectDescription}</p>
+      <div className='project-stuff'>
+        <div className='image-slider'>
+          <div className='buttons'>
+            <button className='left-button' /> {/* onClick={add movement to left} */}
+            <button className='right-button' /> {/* onClick={add movement to right} */}
+          </div>
+          <img src={project.projectImage} alt={project.projectName} />
+          {/* here are gonna be an images sliding right to left if there are more than 2 */}
+        </div>
+        <div className='project-info'>
+          <h2>{project.projectName}</h2>
+          <p>{project.projectDescription}</p>
+          Tech stack: <span>{project.projectSkills}</span>
+        </div>
       </div>
     </div>
   )
