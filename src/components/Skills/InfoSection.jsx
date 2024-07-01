eef78a64ed820fe389bac7fffc738d8501bfbd2d
Martin Flasar <christian.flasar@gmail.com>
2024-07-01 20:03:07 +0200

    Updated InfoSection styling. Box shadow for Skill item.

--- src/components/Skills/InfoSection.jsx
+++ src/components/Skills/InfoSection.jsx
@@ -1,31 +1,59 @@
 import React from "react";
 import "../../assets/components/Skills/InfoSection.scss";
 const InfoSection = ({skill}) => {
+	const calcExperienceState = (percent) => {
+		if (percent > 75) {
+			return "Senior";
+		} else if (percent >= 70 && percent <= 75) {
+			return "Medior/Senior";
+		} else if (percent > 50) {
+			return "Medior";
+		} else if (percent >= 45 && percent <= 50) {
+			return "Junior/Medior";
+		} else if (percent > 15) {
+			return "Junior";
+		} else if (percent >= 10 && percent <= 15) {
+			return "Entry-Level/Junior";
+		} else {
+			return "Entry-level";
+		}
+		}
+
 	return (
 		<div className="info-section">
 			<h2>Info</h2>
       {skill ? (
         <div className="info-content">
 				<div className="info-item">
-					<span className="info-label">Skill Name:</span>
+					<span className="info-label">Skill Name</span>
 					<span className="info-value" id="skill-name">
             {skill.skillName}
 					</span>
 				</div>
 				<div className="info-item">
-					<span className="info-label">Time Since Started:</span>
+					<span className="info-label">Time Since Started</span>
 					<span className="info-value" id="skill-time">
           {skill.skillTimeSinceStarted}
 					</span>
 				</div>
 				<div className="info-item">
-					<span className="info-label">Start Date:</span>
+					<span className="info-label">Start Date</span>
 					<span className="info-value" id="skill-start-date">
           {skill.skillStartDate}
 					</span>
 				</div>
 				<div className="info-item">
-					<span className="info-label">Experience:</span>
+					<span className="info-label">Skill Homepage</span>
+					<a
+						className="info-link"
+						id="skill-url"
+						href={skill.skillUrl}
+					>
+						{skill.skillName} Webpage
+					</a>
+				</div>
+				<div className="info-item info-experience-item">
+					<span className="info-label">Experience</span>
 					<div className="progress-bar">
 						<div className="progress" style={{width: skill.skillLevelPercent + "%",
 													backgroundColor: `${
@@ -36,19 +64,12 @@ const InfoSection = ({skill}) => {
 															: "rgba(200, 200, 10, 0.5)"}`
 													}}></div>
 					</div>
+					<div className="experience-state">
+					    {<span>{calcExperienceState(skill.skillLevelPercent)}</span>}
+					</div>
 				</div>
-				<div className="info-item">
-					<span className="info-label">Skill Homepage:</span>
-					<a
-						className="info-link"
-						id="skill-url"
-						href={skill.skillUrl}
-					>
-						{skill.skillName} Webpage
-					</a>
-				</div>
-				<div className="info-item">
-					<span className="info-label">Description:</span>
+				<div className="info-item info-description-item">
+					<span className="info-label">Description</span>
 					<p className="info-description" id="skill-description">
 						{skill.skillDescription}
 					</p>
